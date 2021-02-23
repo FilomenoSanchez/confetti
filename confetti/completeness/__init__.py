2a0a866f1bb097a7c359c12df82e1c7a207a5c01
FilomenoSanchez <filo_san_rod@hotmail.com>
2021-02-23 16:25:37 +0000

    rename core module to completeness

---
+++ confetti/completeness/__init__.py
@@ -0,0 +1,7 @@
+
+def Dataset(*args, **kwargs):
+    """:py:obj:`~confetti.completeness.dataset.Dataset` instance"""
+    from confetti.completeness.dataset import Dataset
+
+    return Dataset(*args, **kwargs)
+
--- confetti/core/__init__.py
+++
@@ -1,7 +0,0 @@
-
-def Dataset(*args, **kwargs):
-    """:py:obj:`~confetti.core.dataset.Dataset` instance"""
-    from confetti.core.dataset import Dataset
-
-    return Dataset(*args, **kwargs)
-
