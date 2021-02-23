3df9ef60a91cd1993f038401ccabbd056ebeaa23
FilomenoSanchez <filo_san_rod@hotmail.com>
2021-02-23 14:26:00 +0000

    reset sweep wavelength

--- confetti/processing/sweeparray.py
+++ confetti/processing/sweeparray.py
@@ -8,17 +8,19 @@ from confetti.processing import Sweep
 class SweepArray(object):
 
     def __init__(self, experiments_fname, workdir, platform="sge", queue_name=None, queue_environment=None,
-                 max_concurrent_nprocs=1):
+                 max_concurrent_nprocs=1, cleanup=False):
         self.experiments_fname = experiments_fname
         self.workdir = workdir
         self.imported_expt = Experiments(experiments_fname)
         self.dials_exe = 'dials'
         self.scripts = []
+        self.sweeps = []
         self.queue_name = queue_name
         self.queue_environment = queue_environment
         self.max_concurrent_nprocs = max_concurrent_nprocs
         self.platform = platform
         self.shell_interpreter = "/bin/bash"
+        self.cleanup = cleanup
         self.logger = logging.getLogger(__name__)
 
     @property
@@ -38,7 +40,7 @@ class SweepArray(object):
     def _other_task_info(self):
         """A dictionary with the extra kwargs for :py:obj:`pyjob.TaskFactory`"""
 
-        info = {'directory': self.workdir, 'shell': self.shell_interpreter}
+        info = {'directory': self.workdir, 'shell': self.shell_interpreter, 'cleanup': self.cleanup}
 
         if self.platform == 'local':
             info['processes'] = self.max_concurrent_nprocs
@@ -63,9 +65,23 @@ class SweepArray(object):
             sweep = Sweep(idx, workdir, imageset)
             sweep.dials_exe = self.dials_exe
             sweep.dump_pickle()
+            self.sweeps.append(sweep)
             self.scripts.append(sweep.script)
+
+        if len(self.scripts) == 0:
+            raise ValueError('No sweeps to process!')
 
         self.logger.info('Processing sweep array')
         with TaskFactory(self.platform, self.scripts, **self._other_task_info) as task:
             task.name = 'sweep-array'
             task.run()
+
+    def reset_wavelength(self, wavelength=None):
+        if wavelength is None:
+            wavelength = Experiments(self.sweeps[0].integrated_experiments).data[0].beam.get_wavelength()
+
+        for sweep in self.sweeps:
+            sweep_experiments = Experiments(sweep.integrated_experiments)
+            for experiment in sweep_experiments.data:
+                experiment.beam.set_wavelength(wavelength)
+            sweep_experiments.data.as_file()
