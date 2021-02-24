a4f940d7e83e384d8bbb21bc2ca93f3217d8e0fc
FilomenoSanchez <filo_san_rod@hotmail.com>
2021-02-24 13:24:49 +0000

    enable sweep exclusion

--- confetti/processing/cluster.py
+++ confetti/processing/cluster.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import confetti.wrappers
+from cached_property import cached_property
 
 
 class Cluster(object):
@@ -16,18 +17,37 @@ class Cluster(object):
         self.sweeps_dir = sweeps_dir
         self.experiments_identifiers = None
         self.nclusters = None
+        self.exclude_sweeps = []
+
+    # ------------------ General properties ------------------
 
     @property
     def hklout(self):
         return os.path.join(self.workdir, 'merged_FREE.mtz')
 
-    @property
+    @cached_property
     def input_fnames(self):
-        return os.path.join(self.sweeps_dir, 'sweep_*', 'integrated*')
+
+        expt_list = []
+        refl_list = []
+
+        for sweep_dir in os.listdir(self.sweeps_dir):
+            if sweep_dir in self.exclude_sweeps:
+                continue
+            reflections_fname = os.path.join(self.sweeps_dir, sweep_dir, 'integrated.refl')
+            experiments_fname = os.path.join(self.sweeps_dir, sweep_dir, 'integrated.expt')
+
+            if os.path.isfile(reflections_fname) and os.path.isfile(experiments_fname):
+                expt_list.append(experiments_fname)
+                refl_list.append(reflections_fname)
+
+        return expt_list + refl_list
 
     def make_workdir(self):
         if not os.path.isdir(self.workdir):
             os.mkdir(self.workdir)
+
+    # ------------------ General Methods ------------------
 
     def process(self):
         self.make_workdir()
@@ -37,7 +57,7 @@ class Cluster(object):
                                                    self.clustering_threshold, self.nprocs)
         dials_cosym.run()
         if dials_cosym.error:
-            self.logger.error('Cluster {} failed to run cosym'.format(self.id))
+            self.logger.error('Cluster_{} failed to run cosym'.format(self.id))
             self.error = True
             return
         self.experiments_identifiers = dials_cosym.cluster_experiment_identifiers
@@ -46,7 +66,7 @@ class Cluster(object):
         dials_resolution = confetti.wrappers.DialsEstimateResolution(self.workdir, 'symmetrized.*', self.dials_exe)
         dials_resolution.run()
         if dials_resolution.error:
-            self.logger.error('Cluster {} failed to estimate resolution'.format(self.id))
+            self.logger.error('Cluster_{} failed to estimate resolution'.format(self.id))
             self.error = True
             return
 
@@ -54,20 +74,20 @@ class Cluster(object):
                                                    dials_resolution.resolution, self.dials_exe, self.nprocs)
         dials_scale.run()
         if dials_scale.error:
-            self.logger.error('Cluster {} failed to scale'.format(self.id))
+            self.logger.error('Cluster_{} failed to scale'.format(self.id))
             self.error = True
             return
 
         dials_merge = confetti.wrappers.DialsMerge(self.workdir, 'scaled.*', self.dials_exe)
         dials_merge.run()
         if dials_merge.error:
-            self.logger.error('Cluster {} failed to merge'.format(self.id))
+            self.logger.error('Cluster_{} failed to merge'.format(self.id))
             self.error = True
             return
 
         freerflag = confetti.wrappers.FreeRFlag(self.workdir, 'merged.mtz', 'merged_FREE.mtz')
         freerflag.run()
         if freerflag.error:
-            self.logger.error('Cluster {} failed to create FREE flag'.format(self.id))
+            self.logger.error('Cluster_{} failed to create FREE flag'.format(self.id))
             self.error = True
             return
