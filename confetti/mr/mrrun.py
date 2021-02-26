a5404681d659d33674e45cc55ee422af239ca46e
FilomenoSanchez <filo_san_rod@hotmail.com>
2021-02-26 11:04:20 +0000

    added mrrun.py

--- confetti/mr/mrrun.py
+++ confetti/mr/mrrun.py
@@ -1,20 +1,68 @@
 import os
+import pickle
+import logging
 from confetti.mr import MtzParser
+from confetti.wrappers import Phaser, Refmac, Buccaneer
 
 
 class MrRun(object):
 
-    def __init__(self, id, workdir, mtz_fname, mw, phaser_stdin, refmac_stdin, buccaneer_stdin):
+    def __init__(self, id, workdir, mtz_fname, mw, phaser_stdin, refmac_stdin, buccaneer_keywords):
         self.id = id
         self.mtz_fname = mtz_fname
         self.workdir = os.path.join(workdir, 'mr_{}'.format(id))
-        self.phaser_stdin = phaser_stdin
+        self.pickle_fname = os.path.join(self.workdir, 'mrrun.pckl')
+        self.phaser = None
         self.refmac_stdin = refmac_stdin
-        self.buccaneer_stdin = buccaneer_stdin
+        self.refmac = None
+        self.buccaneer = None
+        self.buccaneer_keywords = buccaneer_keywords
         self.mw = mw
         self.mtz_parser = MtzParser(self.mtz_fname)
         self.mtz_parser.parse()
         self.ncopies, self.solvent = self.estimate_contents(self.mtz_parser.reflection_file.cell.volume_per_image(), mw)
+        self.phaser_stdin = phaser_stdin.format(**{'COPIES': self.ncopies, 'MW': self.mw})
+        self.logger = logging.getLogger(__name__)
+
+        # ------------------ Class methods ------------------
+
+    @classmethod
+    def from_pickle(cls, pickle_fname):
+        with open(pickle_fname, 'rb') as fhandle:
+            return pickle.load(fhandle)
+
+    # ------------------ General methods ------------------
+
+    def dump_pickle(self):
+        self.make_workdir()
+        with open(self.pickle_fname, 'wb') as fhandle:
+            pickle.dump(self, fhandle)
+
+    def make_workdir(self):
+        if not os.path.isdir(self.workdir):
+            os.mkdir(self.workdir)
+
+    def run(self):
+        self.phaser = Phaser(self.workdir, self.phaser_stdin)
+        self.phaser.run()
+        if self.phaser.error:
+            self.logger.error('MR-Run {} failed to execute phaser'.format(self.id))
+            return
+
+        self.refmac = Refmac(self.workdir, self.mtz_fname, 'refmac_out.mtz',
+                             self.phaser.expected_output, 'refmac_out.pdb', self.refmac_stdin)
+        self.refmac.run()
+        if self.refmac.error:
+            self.logger.error('MR-Run {} failed to execute refmac'.format(self.id))
+            return
+
+        self.buccaneer = Buccaneer(self.workdir, self.buccaneer_keywords)
+        self.buccaneer.run()
+        if self.buccaneer.error:
+            self.logger.error('MR-Run {} failed to execute buccaneer'.format(self.id))
+            return
+
+    # ------------------ Static methods ------------------
 
     @staticmethod
     def estimate_contents(cell_volume, mw):
