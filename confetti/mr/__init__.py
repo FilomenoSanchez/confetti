d587f211afeaaef2d63d754311e409c57d038dab
FilomenoSanchez <filo_san_rod@hotmail.com>
2021-03-09 19:35:03 +0000

    add hklimport to __init__.py

--- confetti/mr/__init__.py
+++ confetti/mr/__init__.py
@@ -12,6 +12,13 @@ def MrArray(*args, **kwargs):
     return MrArray(*args, **kwargs)
 
 
+def HklImport(*args, **kwargs):
+    """:py:obj:`~confetti.mr.hklimport.HklImport` instance"""
+    from confetti.mr.hklimport import HklImport
+
+    return HklImport(*args, **kwargs)
+
+
 def MtzParser(*args, **kwargs):
     """:py:obj:`~confetti.mr.mtzparser.MtzParser` instance"""
     from confetti.mr.mtzparser import MtzParser
