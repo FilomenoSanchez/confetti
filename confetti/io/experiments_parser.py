ed652ae24c24c36d39a8370c889f9da6f9182361
FilomenoSanchez <filo_san_rod@hotmail.com>
2021-05-13 17:45:53 +0100

    fix sweep slicing angles

--- confetti/io/experiments_parser.py
+++ confetti/io/experiments_parser.py
@@ -1,6 +1,6 @@
 import random
 from confetti.io.parser import Parser
-from dials.array_family import flex
+from dials.utils import slice_experiments
 from dials.util.phil import FilenameDataWrapper
 from dxtbx.model.experiment_list import ExperimentListFactory
 
@@ -9,7 +9,17 @@ class Experiments(Parser):
 
     def _parse(self):
         self.data = self.read_experiments(self.fname)
+        self._register_data()
 
+    @staticmethod
+    def read_experiments(fname):
+        wrapper = FilenameDataWrapper(
+            filename=fname,
+            data=ExperimentListFactory.from_json_file(fname, check_format=True)
+        )
+        return wrapper.data
+
+    def _register_data(self):
         self.templates = []
         for imageset in self.data.imagesets():
             self.templates.append(imageset.get_template())
@@ -28,19 +38,12 @@ class Experiments(Parser):
         for experiment in self.data:
             self.identifiers.append(experiment.identifier)
 
-    @staticmethod
-    def read_experiments(fname):
-        wrapper = FilenameDataWrapper(
-            filename=fname,
-            data=ExperimentListFactory.from_json_file(fname, check_format=True)
-        )
-        return wrapper.data
-
     def slice_experiments(self, angle_threshold, discard_sweeps_outside=False, random_start=False):
         identifiers_to_remove = []
+        new_image_ranges = []
         for experiment in self.data:
-            image_range = experiment.scan.get_image_range()
-            angle_range = [experiment.scan.get_angle_from_image_index(idx) for idx in range(1, experiment.imageset.size()+1)]
+            angle_range = [experiment.scan.get_angle_from_image_index(i) for i in
+                           range(1, experiment.imageset.size() + 1)]
             initial_angle = angle_range[0]
             final_angle = angle_range[-1]
             total_angle_gain = abs(initial_angle - final_angle)
@@ -51,7 +54,7 @@ class Experiments(Parser):
             elif not random_start:
                 for idx, angle in enumerate(angle_range, 1):
                     if abs(angle - initial_angle) >= angle_threshold and len(range(0, idx)) != 0:
-                        experiment.scan.set_image_range((1, idx))
+                        new_image_ranges.append((1, idx))
                         break
             else:
                 last_valid_start_idx = None
@@ -68,10 +71,12 @@ class Experiments(Parser):
                     initial_angle = angle_range[start]
                     for idx, angle in enumerate(angle_range[start:], start + 1):
                         if abs(angle - initial_angle) >= angle_threshold and len(range(start, idx)) != 0:
-                            experiment.scan.set_image_range((start+1, idx))
+                            new_image_ranges.append((start + 1, idx))
                             break
                 else:
                     identifiers_to_remove.append(experiment.identifier)
 
         if any(identifiers_to_remove):
             self.data.remove_on_experiment_identifiers(identifiers_to_remove)
+        self.data = slice_experiments(self.data, [new_image_ranges])
+        self._register_data()
